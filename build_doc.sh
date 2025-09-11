c9cb991b884e1514d279bc58065838521b9d5938
ruichen <huangruichen@lejurobot.com>
2025-09-11 14:41:37 +0800

    netlify: use bash + build to docs/_build/html

--- build_doc.sh
+++ build_doc.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-python --version || true
 
 python -m pip install -U pip
 python -m pip install -r requirements.txt
 
-rm -rf docs/build
-mkdir -p docs/build/html
-sphinx-build -b html -D language=zh_CN docs/source docs/build/html
 
-touch docs/build/html/.nojekyll
+rm -rf docs/_build
+mkdir -p docs/_build/html
+sphinx-build -b html -D language=zh_CN docs/source docs/_build/html
+
+touch docs/_build/html/.nojekyll
 
