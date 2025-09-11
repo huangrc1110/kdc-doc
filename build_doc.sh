f2e6f6ae2b0a8d98f7886064ec17bcbd3c5fa381
ruichen <huangruichen@lejurobot.com>
2025-09-11 14:37:49 +0800

    add deps install & sphinx build for Netlify

--- build_doc.sh
+++ build_doc.sh
@@ -1,4 +1,14 @@
-# Building document command
-rm -R docs/build
-mkdir docs/build
+#!/usr/bin/env bash
+set -euo pipefail
+
+python --version || true
+
+python -m pip install -U pip
+python -m pip install -r requirements.txt
+
+rm -rf docs/build
+mkdir -p docs/build/html
 sphinx-build -b html -D language=zh_CN docs/source docs/build/html
+
+touch docs/build/html/.nojekyll
+
